9afcb53e5b4cac5ecb6dbfa6a5e174cb99077957
Lisso-lab <85261069+Lisso-lab@users.noreply.github.com>
2022-08-21 08:33:28 +0000

    Create module.lua

---
+++ module.lua
@@ -0,0 +1,71 @@
+--!strict
+local tween_service: TweenService = game:GetService("RunService")
+
+local function do_options(tabl, options)
+	if type(tabl) ~= "table" then
+		tabl = options
+	else
+		for i,v in pairs(options) do
+			local val do
+				if type(tabl[i]) ~= "nil" then
+					val = tabl[i]
+				else
+					val = options[i]
+				end
+			end
+	
+			tabl[i] = val
+		end
+	end
+
+	return tabl
+end
+
+local funcs = {
+    Version = "1.0.0"
+}
+
+funcs.get_in = function(inst: Instance, options)
+	options = do_options(options,
+		{
+			do_desc = false,
+			do_isa = true,
+			isa = {"BasePart"}
+		}
+	)
+
+	local tabl = {}
+
+	local get = options.do_desc and inst:GetDescendants() or inst:GetChildren()
+
+	for _,v in pairs(get) do
+		local v_lower = string.lower(v.Name)
+
+		if options.do_isa == true then
+			for i=1,#options.isa do
+				if v:IsA(options.isa[i]) then tabl[#tabl+1] = v end
+			end
+		elseif table.find(options.names,v_lower) then
+			tabl[#tabl+1] = v
+		end
+	end
+
+	return tabl
+end
+
+funcs.tween = function(inst: Instance,tween_info: TweenInfo,props)
+    local tween_info: TweenInfo = tween_info or TweenInfo.new(
+        .1,
+        Enum.EasingStyle.Linear,
+        Enum.EasingDirection.In,
+        0,
+        false,
+        0
+    )
+
+    local tween: Tween = tween_service:Create(inst,tween_info,props)
+    coroutine.wrap(function() tween:Play() end)()
+end
+
+
+return funcs
